5ee8139082b25f87867225c1ee26cd0e92a816cc
EricForm <infodevweb67@gmail.com>
2021-07-25 17:19:57 +0200

    Espace membre : page de configuration de compte

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,12 +3,21 @@
 namespace App\Controllers;
 
 use SquareMvc\Foundation\AbstractController;
+use SquareMvc\Foundation\Authentication as Auth;
 use SquareMvc\Foundation\View;
 
 class HomeController extends AbstractController
 {
     public function index(): void
     {
+        if (!Auth::check()) {
+            $this->redirect('login.form');
+        }
 
+        $user = Auth::get();
+
+        View::render('home', [
+            'user' => $user,
+        ]);
     }
 }
